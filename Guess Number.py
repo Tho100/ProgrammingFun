351014ec914f38f16278ce728824d3c21aaa111f
Danny <64541739+Tho100@users.noreply.github.com>
2021-12-30 15:34:13 +0800

    Update Guess Number.py

--- Guess Number.py
+++ Guess Number.py
@@ -6,7 +6,7 @@ import random
 def num_generator(n):
     a = random.randint(1,6)
     if(n == a):
-        print('fuck you')
+        print("GG")
 
 # Define player function    
 
@@ -14,11 +14,11 @@ def player_func():
     t = 5
     while t > 0:
         t -= 1
-        ply = int(input("> "))
+        ply = int(input("> "))  
         num_generator(ply)
         # * End the game if the user reached 5 inputs 
         if(t == 0):
             print("Game Ended")
-            
+
 # * Call game func
 player_func()
